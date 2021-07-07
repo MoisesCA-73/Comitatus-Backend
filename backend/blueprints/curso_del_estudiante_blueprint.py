51c6bda1d890043001b342589d6cb8791cac5aea
MoisesCA-73 <66378253+MoisesCA-73@users.noreply.github.com>
2021-07-07 17:21:31 -0500

    Update curso_del_estudiante_blueprint.py

--- backend/blueprints/curso_del_estudiante_blueprint.py
+++ backend/blueprints/curso_del_estudiante_blueprint.py
@@ -11,6 +11,7 @@ curso_del_estudiante_blueprint = Blueprint('curso_del_estudiante_blueprint', __n
 
 model = CursoDelEstudianteModel()
 
+#Ruta para crear cursos en la tabla cursos del estudiante
 @curso_del_estudiante_blueprint.route('/curso_del_estudiante/create_curso_del_estudiante', methods=['POST'])
 @cross_origin()
 def create_curso_del_estudiante():
@@ -18,16 +19,19 @@ def create_curso_del_estudiante():
     return jsonify(content)
 
 
+#Ruta para borrar cursos en la tabla cursos del estudiante
 @curso_del_estudiante_blueprint.route('/curso_del_estudiante/delete_curso_del_estudiante', methods=['POST'])
 @cross_origin()
 def delete_curso_del_estudiante():
     return jsonify(model.delete_curso_del_estudiante(int(request.json['cui'])))
 
+#Ruta para crear cursos de un solo estudiante en la tabla cursos del estudiante
 @curso_del_estudiante_blueprint.route('/curso_del_estudiante/get_curso_del_estudiante', methods=['POST'])
 @cross_origin()
 def curso_del_estudiante():
     return jsonify(model.get_curso_del_estudiante(int(request.json['cui'])))
 
+#Ruta para retornar todos los cursos de todos los estudiantes en la tabla cursos del estudiante
 @curso_del_estudiante_blueprint.route('/curso_del_estudiante/get_all_curso_del_estudiante', methods=['POST'])
 @cross_origin()
 def all_curso_del_estudiante():
